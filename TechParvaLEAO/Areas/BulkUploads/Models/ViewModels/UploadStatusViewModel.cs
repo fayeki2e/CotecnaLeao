2c2a9e2ddf7d50357d0084f0882b743edb9a6c3e
PriyaMishra <priyatiwarijune2023@gmail.com>
2024-01-01 23:24:44 +0530

    Bulk Upload changes

---
+++ TechParvaLEAO/Areas/BulkUploads/Models/ViewModels/UploadStatusViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MediatR;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using UoN.ExpressiveAnnotations.NetCore.Attributes;
+
+namespace TechParvaLEAO.Models
+{
+    /*
+     * View model for Upload
+     */
+
+    public class UploadStatusViewModel {
+      
+
+        public UploadStatusViewModel()
+        {
+            StatusListItems = new List<StatusViewModel>();
+        }
+        public virtual IList<StatusViewModel> StatusListItems { get; set; }
+    }
+
+    public class StatusViewModel
+    {
+        public int Row_ID { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "UploadedOn")]
+        public DateTime UploadedOn { get; set; }
+        public string UploadedBy { get; set; }
+       public int AddedRecords { get; set; }
+        public int UpdatedRecords { get; set; }
+        public int FailedRecords { get; set; }
+    }
+
+
+
+
+}
